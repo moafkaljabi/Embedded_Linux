cab4091dbb7f13015536d048f985302d36829f55
Moafk Aljabi <Mwafak_sh@hotmail.com>
2024-07-16 21:10:59 +0200

    python

---
+++ c++/basics/first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void toPrint(std::string name){
+    // std::cout << "Enter your name: " << std::endl;
+    // std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
+}
+
+
+int main(){
+    toPrint("Moafk");
+    return 0;
+}
