fc6db3305ebb954db5c51774794c8a8abe52e9b1
Moafk Aljabi <Mwafak_sh@hotmail.com>
2024-08-07 01:11:38 +0200

    car API

--- c++/basics/carAPI.cpp
+++ c++/basics/carAPI.cpp
@@ -1,50 +1,59 @@
 #include <iostream>
-#include <string> 
-#include <optional>  
+#include <string>
+#include <optional>
 #include <vector>
 
-// Define the car class
-class car {
+// Define the Car class
+class Car {
 public:
     std::string make;
     std::string model;
     int year;
-    std::optional<std::string> color;  // Use std::optional for optional color
+    std::optional<std::string> color;
 
-    // Constructor with optional color
-    car(const std::string &make, const std::string &model, int year, std::optional<std::string> color = std::nullopt)
-        : make(make), model(model), year(year), color(color) {}
+    // Constructor with member initializer list
+    // Initializes member variables directly using constructor parameters
+    Car(const std::string &carMake,
+        const std::string &carModel,
+        int carYear,
+        std::optional<std::string> carColor = std::nullopt)
+        : make(carMake), model(carModel), year(carYear), color(carColor) {}
 };
 
-// Define the global vector to store car objects after the car class definition
-std::vector<car> cars;  
+// Define the global vector to store Car objects after the Car class definition
+std::vector<Car> Cars;
+
+
+// Function to print all cars
+void printCars() {
+    for (const Car &car : Cars) {
+        std::cout << "Make: " << car.make << ", Model: " << car.model << ", Year: " << car.year;
+        if (car.color) {
+            std::cout << ", Color: " << *car.color;
+        }
+        std::cout << std::endl;
+    }
+}
+
+
 
 // Function to print a string (not used in the current main.cpp, but kept here for completeness)
 void toPrint(const std::string &input) {
     std::cout << input << std::endl;
 }
 
-// Function to print all cars
-void printCars() {
-    for (const auto &c : cars) {
-        std::cout << "Make: " << c.make << ", Model: " << c.model << ", Year: " << c.year;
-        if (c.color) {
-            std::cout << ", Color: " << *c.color;
-        }
-        std::cout << std::endl;
-    }
-}
 
 // Function to add a car to the list
-void addCar(const car &newCar) {
-    cars.push_back(newCar);
+void addCar(const Car &newCar) {
+    Cars.push_back(newCar);
 }
 
 // Function to delete a car from the list by index
 void deleteCar(size_t index) {
-    if (index < cars.size()) {
-        cars.erase(cars.begin() + index);
+    if (index < Cars.size()) {
+        Cars.erase(Cars.begin() + index);
     } else {
         std::cout << "Invalid index" << std::endl;
     }
 }
+
--- c++/basics/first.cpp
+++ c++/basics/first.cpp
@@ -4,9 +4,9 @@
 
 int main() {
     // Add some cars
-    addCar(car("Ford", "Mustang", 2023, "Red"));
-    addCar(car("Toyota", "Camry", 2021));
-    addCar(car("Honda", "Civic", 2022, "Blue"));
+    addCar(Car("Ford", "Mustang", 2023, "Red"));
+    addCar(Car("Toyota", "Camry", 2021));
+    addCar(Car("Honda", "Civic", 2022, "Blue"));
 
     toPrint("Initial car list:");
     printCars();
