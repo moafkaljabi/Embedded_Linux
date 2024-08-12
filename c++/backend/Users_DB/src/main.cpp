39c29b45e12c8413dd1096fe85d975efd32bd07f
Moafk Aljabi <Mwafak_sh@hotmail.com>
2024-08-12 22:38:09 +0200

    -m c++ bascis review

--- c++/backend/Users_DB/src/main.cpp
+++ c++/backend/Users_DB/src/main.cpp
@@ -2,7 +2,7 @@
 #include <mysql_driver.h>
 #include <mysql_connection.h>
 #include <cppconn/statement.h> // Include this for sql::Statement
-#include "myKey.h"
+#include "../config.h"
 
 int main() {
     sql::mysql::MySQL_Driver *driver;
@@ -10,14 +10,14 @@ int main() {
 
     try {
         driver = sql::mysql::get_mysql_driver_instance();
-        con = driver->connect("tcp://127.0.0.1:3306", username, password); // Update credentials if needed
+        con = driver->connect(DB_HOST, DB_USER, DB_PASSWORD); // Update credentials if needed
 
-        con->setSchema("Users"); // Replace with your database name
+        con->setSchema(DB_NAME); // Replace with your database name
 
         sql::Statement *stmt = con->createStatement();
 
         // Example: Insert a new device
-        std::string insertQuery = "INSERT INTO Devices (PersonID, DeviceName, DeviceStatus) VALUES (1, 'BLDC2 Motor', 'on')";
+        std::string insertQuery = "INSERT INTO Devices (PersonID, DeviceName, DeviceStatus) VALUES (1, 'BLDC693 Motor', 'on')";
         stmt->execute(insertQuery);
 
         // Example: Update device status
