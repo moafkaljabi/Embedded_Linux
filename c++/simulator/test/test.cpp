e07baa03b7a5a95fcc65aefc3d9312925aacf68b
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-03-12 04:38:00 +0100

    Developing modern C++

---
+++ c++/simulator/test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+
+std::jthread thread([] {
+    std::cout << "Hello from thread!" << std::endl;
+});
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
